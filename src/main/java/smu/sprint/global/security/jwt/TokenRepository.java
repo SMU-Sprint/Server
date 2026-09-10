1b5b0673c422d31d691798d8d1ff9b9faad437f3
Hanmin <rlemdhs2747@naver.com>
2026-09-10 15:32:42 +0900

    ✨ feat: JWT 엔티티 및 관련 기능 추가

---
+++ src/main/java/smu/sprint/global/security/jwt/TokenRepository.java
@@ -0,0 +1,14 @@
+package smu.sprint.global.security.jwt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import smu.sprint.domain.entity.Member;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    Optional<Token> findByMember(Member member);
+
+}
